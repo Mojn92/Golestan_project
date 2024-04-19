e3ce327ee5ca7c5deb3962d163e46334e8be733b
mojtaba <monjedu92@gmail.com>
2024-04-19 18:51:44 +0330

    videos

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,15 @@ export default {
   content: ["./index.html"],
   theme: {
     screens: {
+      'mobile1':'0px',
+      // => @media (min-width: 0px) { ... }
       'tablet1': '768px',
       // => @media (min-width: 768px) { ... }
 
       'laptop1': '1024px',
       // => @media (min-width: 1024px) { ... }
 
-      'desktop1': '1280px',
+      'desktop1': '1366px',
       // => @media (min-width: 1280px) { ... }
     },
   },
