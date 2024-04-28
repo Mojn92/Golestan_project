03da32621401045a42240133bee5ee356f7acc24
mojtaba <monjedu92@gmail.com>
2024-04-28 15:19:24 +0330

    weblogproducts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html"],
+  content: ["./*.html"],
   theme: {
     screens: {
-      'mobile1':'0px',
-      // => @media (min-width: 0px) { ... }
       'tablet1': '768px',
       // => @media (min-width: 768px) { ... }
-
+      
+                                                    
       'laptop1': '1024px',
       // => @media (min-width: 1024px) { ... }
 
